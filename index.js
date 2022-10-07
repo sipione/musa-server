6a08e437528065e2117cef00fc1d09eccc093748
Ricardo Sipione <ricardisipione@MacBook-Pro-de-Ricardi.local>
2022-10-07 17:14:28 -0300

    bora4

--- index.js
+++ index.js
@@ -22,36 +22,3 @@ const port = process.env.PORT || 8080
 app.listen(port, () => {
   console.log(`App listening`)
 })
-
-
-/*const nodemailer = require("nodemailer");
-
-const send = async ()=>{
-    let transporter = nodemailer.createTransport({
-        host: "smtp.hostinger.com",
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-          user: "ricardo@sipione.tech", // generated ethereal user
-          pass: "Sipione@0058", // generated ethereal password
-        },
-      });
-      
-      try{
-        await transporter.sendMail({
-            from: '"BORA BILLL 👻" <ricardo@sipione.tech>', // sender address
-            to: "ricardosipe@gmail.com", // list of receivers
-            subject: "Hello ✔", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>", // html body
-          });
-
-          console.log("enviado")
-      }catch(err){
-        console.log(err)
-      }
-      
-
-}
-
-send(*/
